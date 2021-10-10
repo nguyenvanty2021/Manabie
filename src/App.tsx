07c30412ea75eedcf720538ffce58ffb3cde5074
nguyenvanty2021 <ty.nguyen@unibiz.io>
2021-10-10 13:46:50 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { RouteComponentProps } from 'react-router'
+import routes from './config/routes'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="App">
+      <BrowserRouter>
+        <Switch>
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                render={(props: RouteComponentProps<any>) => (
+                  <route.component {...props} {...route.props} />
+                )}
+              />
+            )
+          })}
+          {/* <Route path="/todo" component={ToDoPage}/> */}
+        </Switch>
+      </BrowserRouter>
+    </main>
+  )
 }
 
-export default App;
+export default App
